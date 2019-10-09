f891264694f83e97eb4ed7eb1e42d58905def1ea
Manoel Quirino <manoelquirinoneto@gmail.com>
2019-10-09 11:34:40 -0300

    initial commit

---
+++ lib/amqp_batch/version.rb
@@ -0,0 +1,3 @@
+module AmqpBatch
+  VERSION = "0.1.0"
+end
---
+++ spec/amqp_batch_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe AmqpBatch do
+  it "has a version number" do
+    expect(AmqpBatch::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
